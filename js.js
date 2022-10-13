c34243bc3479289b4f92f49f1f142dce0d399ff0
Masre87 <m.masre2021@gmail.com>
2022-10-13 21:53:53 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,13 @@
+
+// prompt("how old are you")
+var age=25;
+// console.log("test");
+// console.error("no error");
+// console.warn("warning");
+
+console.log(age);
+function test;
+var name =(memo);
+memo =35
+console.log=35
+
